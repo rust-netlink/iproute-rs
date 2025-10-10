195496087859c87720f1c03aaa881dc7b134a7d4
Enrique Llorente <ellorent@redhat.com>
2025-10-10 10:17:38 +0200

    run test on network namespace
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- src/ip/link/tests/link.rs
+++ src/ip/link/tests/link.rs
@@ -2,29 +2,54 @@
 
 use crate::tests::{exec_cmd, get_ip_cli_path};
 
+const TEST_NETNS: &str = "iproute-rs-test";
+
+/// Execute a command inside the test network namespace
+fn exec_in_netns(args: &[&str]) -> String {
+    let mut full_args = vec!["ip", "netns", "exec", TEST_NETNS];
+    full_args.extend_from_slice(args);
+    exec_cmd(&full_args)
+}
+
 #[cfg(test)]
 #[ctor::ctor]
 fn setup() {
-    println!("setup for interfaces for tests");
+    println!("setup network namespace and interfaces for tests");
+
+    // Create network namespace (delete first if it exists)
+    let netns_list = exec_cmd(&["ip", "netns", "list"]);
+    if netns_list.contains(TEST_NETNS) {
+        exec_cmd(&["ip", "netns", "del", TEST_NETNS]);
+    }
+    exec_cmd(&["ip", "netns", "add", TEST_NETNS]);
 
     // Add vlan over dummy interface
-    exec_cmd(&["ip", "link", "add", "link", "dummy0", "type", "dummy"]);
-    exec_cmd(&[
+    exec_in_netns(&["ip", "link", "add", "link", "dummy0", "type", "dummy"]);
+    exec_in_netns(&[
         "ip", "link", "add", "link", "dummy0", "name", "dummy0.1", "type",
         "vlan", "id", "1",
     ]);
 
-    exec_cmd(&["ip", "link", "set", "dummy0", "up"]);
-    exec_cmd(&["ip", "link", "set", "dummy0.1", "up"]);
+    exec_in_netns(&["ip", "link", "set", "dummy0", "up"]);
+    exec_in_netns(&["ip", "link", "set", "dummy0.1", "up"]);
+}
+
+#[cfg(test)]
+#[ctor::dtor]
+fn teardown() {
+    println!("teardown network namespace for tests");
+
+    // Delete network namespace
+    exec_cmd(&["ip", "netns", "del", TEST_NETNS]);
 }
 
 #[test]
 fn test_link_show() {
     let cli_path = get_ip_cli_path();
 
-    let expected_output = exec_cmd(&["ip", "link", "show"]);
+    let expected_output = exec_in_netns(&["ip", "link", "show"]);
 
-    let our_output = exec_cmd(&[cli_path.as_str(), "link", "show"]);
+    let our_output = exec_in_netns(&[cli_path.as_str(), "link", "show"]);
 
     pretty_assertions::assert_eq!(expected_output, our_output);
 }
@@ -33,9 +58,9 @@ fn test_link_show() {
 fn test_link_detailed_show() {
     let cli_path = get_ip_cli_path();
 
-    let expected_output = exec_cmd(&["ip", "-d", "link", "show"]);
+    let expected_output = exec_in_netns(&["ip", "-d", "link", "show"]);
 
-    let our_output = exec_cmd(&[cli_path.as_str(), "-d", "link", "show"]);
+    let our_output = exec_in_netns(&[cli_path.as_str(), "-d", "link", "show"]);
 
     pretty_assertions::assert_eq!(expected_output, our_output);
 }
@@ -44,9 +69,9 @@ fn test_link_detailed_show() {
 fn test_link_show_json() {
     let cli_path = get_ip_cli_path();
 
-    let expected_output = exec_cmd(&["ip", "-j", "link", "show"]);
+    let expected_output = exec_in_netns(&["ip", "-j", "link", "show"]);
 
-    let our_output = exec_cmd(&[cli_path.as_str(), "-j", "link", "show"]);
+    let our_output = exec_in_netns(&[cli_path.as_str(), "-j", "link", "show"]);
 
     pretty_assertions::assert_eq!(expected_output, our_output);
 }
@@ -55,9 +80,10 @@ fn test_link_show_json() {
 fn test_link_detailed_show_json() {
     let cli_path = get_ip_cli_path();
 
-    let expected_output = exec_cmd(&["ip", "-d", "-j", "link", "show"]);
+    let expected_output = exec_in_netns(&["ip", "-d", "-j", "link", "show"]);
 
-    let our_output = exec_cmd(&[cli_path.as_str(), "-d", "-j", "link", "show"]);
+    let our_output =
+        exec_in_netns(&[cli_path.as_str(), "-d", "-j", "link", "show"]);
 
     pretty_assertions::assert_eq!(expected_output, our_output);
 }
